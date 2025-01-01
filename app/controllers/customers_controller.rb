d5006864ec624068a51fd9dbc97f78abb90f4b7d
Thanh Viet <thanhviet.voyager@gmail.com>
2025-01-01 21:03:58 +0700

    feature: Add the scheduled job, recurring job, and jobs management UI web

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -7,8 +7,16 @@ class CustomersController < ApplicationController
 
   def export_csv
     user_email = params[:email]
-    ExportCustomersCsvJob.perform_later(user_email)
-    flash[:success] = "CSV export is being processed. You will receive an email when it is ready."
+    export_time = params[:export_time]
+    if export_time.present?
+      time_to_run = Time.parse(export_time)
+      ExportCustomersCsvJob.set(wait_until: time_to_run).perform_later(user_email)
+      flash[:success] = "CSV export will process at #{time_to_run.strftime('%Y-%m-%d %H:%M:%S')}."
+    else
+      ExportCustomersCsvJob.perform_later(user_email)
+      flash[:success] = "CSV export is being processed. You will receive an email when it is ready."
+    end
+
     redirect_to customers_path
   end
 end

56aca4957a4aabee4a1ff341420df821ed006498
Thanh Viet <thanhviet.voyager@gmail.com>
2025-11-25 09:29:08 +0700

    feature: Fix the 'Method has too many lines' Rubocop warning in the 'CustomersController' controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,13 +1,11 @@
 class CustomersController < ApplicationController
-  layout 'solid-queue'
+  layout "solid-queue"
 
   def index
     @customers = Customer.page(params[:page])
   end
 
   def export_csv
-    user_email = params[:email]
-    export_time = params[:export_time]
     if export_time.present?
       time_to_run = Time.parse(export_time)
       ExportCustomersCsvJob.set(wait_until: time_to_run).perform_later(user_email)
@@ -16,7 +14,16 @@ class CustomersController < ApplicationController
       ExportCustomersCsvJob.perform_later(user_email)
       flash[:success] = 'CSV export is being processed. You will receive an email when it is ready.'
     end
-
     redirect_to customers_path
+  end
+
+  private
+
+  def user_email
+    params[:email]
+  end
+
+  def export_time
+    params[:export_time]
   end
 end
